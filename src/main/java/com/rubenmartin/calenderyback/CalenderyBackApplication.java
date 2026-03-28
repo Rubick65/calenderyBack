fc7b852e708ec059ddf2a481081419fc2bd12c88
Ruben <martinandraderuben@gmail.com>
2026-03-28 12:50:49 +0100

    Initial commit

---
+++ src/main/java/com/rubenmartin/calenderyback/CalenderyBackApplication.java
@@ -0,0 +1,13 @@
+package com.rubenmartin.calenderyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubenmartin/calenderyback/CalenderyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenmartin.calenderyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
