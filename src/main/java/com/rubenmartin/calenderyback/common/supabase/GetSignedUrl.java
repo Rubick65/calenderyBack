7adcf2c1b69c2a794268be330126bdb1d886867e
ruben <martinandraderuben@gmail.com>
2026-04-28 16:15:27 +0200

    fix: resolve bug that dont´t let´s get any signedUrl in production

--- src/main/java/com/rubenmartin/calenderyback/common/supabase/GetSignedUrl.java
+++ src/main/java/com/rubenmartin/calenderyback/common/supabase/GetSignedUrl.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Component
 @Getter
 public class GetSignedUrl {
 
