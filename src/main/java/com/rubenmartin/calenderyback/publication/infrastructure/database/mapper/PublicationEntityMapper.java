e6cdbb7aecb6ceea0a00b3ec2052a526f5d811a3
ruben <martinandraderuben@gmail.com>
2026-04-29 12:25:47 +0200

    fix: resolve infinity recursive loop in when mapping PublicationEntity

--- src/main/java/com/rubenmartin/calenderyback/publication/infrastructure/database/mapper/PublicationEntityMapper.java
+++ src/main/java/com/rubenmartin/calenderyback/publication/infrastructure/database/mapper/PublicationEntityMapper.java
@@ -3,13 +3,16 @@ package com.rubenmartin.calenderyback.publication.infrastructure.database.mapper
 import com.rubenmartin.calenderyback.publication.domain.entity.Publication;
 import com.rubenmartin.calenderyback.publication.infrastructure.database.entity.PublicationEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface PublicationEntityMapper {
 
+    @Mapping(target = "publicationDate.publication", ignore = true)
     PublicationEntity mapToPublicationEntity(Publication publication);
 
+    @Mapping(target = "publicationDate.publication", ignore = true)
     Publication mapToPublication(PublicationEntity publicationEntity);
 }
